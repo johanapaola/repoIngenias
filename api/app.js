7e743d4369258ab0cc6866e9b3997f47485a3cfa
lucianazara <55200459+lucianazara@users.noreply.github.com>
2023-08-20 07:26:16 -0300

    commented out code in duplicated server

--- api/app.js
+++ api/app.js
@@ -1,19 +1,19 @@
-const http = require('http');
-const express = require('express');
-const server = express();
-const PORT = process.env.PORT || 3008;
-const routes = require('./routes/index');
+// const http = require('http');
+// const express = require('express');
+// const server = express();
+// const PORT = process.env.PORT || 3008;
+// const routes = require('./routes/index');
 
-server.use('/', routes);
+// server.use('/', routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
+// // Error catching endware.
+// server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+//   const status = err.status || 500;
+//   const message = err.message || err;
+//   console.error(err);
+//   res.status(status).send(message);
+// });
 
-server.listen(PORT, () => {
-    console.log('%s listening at 3008'); // eslint-disable-line no-console
-  });
+// server.listen(PORT, () => {
+//     console.log('%s listening at 3008'); // eslint-disable-line no-console
+//   });
